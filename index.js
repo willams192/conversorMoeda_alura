696c3ada578ac0c8ebc761c061a51457284e273d
willams192 <wiilhams82@gmail.com>
2023-11-13 17:18:26 -0300

    Adicionando conversor de criptomoeda

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var valorDolar = 30;
-alert(valorDolar)
+let valueReal = parseFloat(prompt('Digite o valor em reais:'));
 
+if (isNaN(valueReal)) {
+    alert('Por favor, insira um valor numérico válido.');
+} else {
+    let btcExchangeRate = 0.00002;
+
+    let valueBtc = valueReal * btcExchangeRate;
+
+    alert('BTC ' + valueBtc.toFixed(8));
+}
